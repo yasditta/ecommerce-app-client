9fe4adbe8b40e82ad3e8af7aaedcc19e0ebef297
tahir kalim <thrkalim@gmail.com>
2021-10-10 00:23:36 +0200

    cartjs without context API

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import Header from './Header'
+import Main from './Main'
+import Basket from './Basket'
+import data from './data'
+import { useState } from 'react'
 
 function Cart() {
-    return (
-        <div>
-            Cart page
-        </div>
-    )
-}
+  const { products } = data
+  const [cartItems, setCartItems] = useState([])
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id)
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x,
+        ),
+      )
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }])
+    }
+  }
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id)
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id))
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x,
+        ),
+      )
+    }
+  }
+  return (
+    <div className='App'>
+      <Header countCartItems={cartItems.length}></Header>
+      <div className='row'>
+        <Main products={products} onAdd={onAdd}></Main>
 
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}></Basket>
+      </div>
+    </div>
+  )
+}
 export default Cart
---
+++ src/pages/cart/data.js
@@ -0,0 +1,24 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Fits 15 Laptops',
+      price: 1100,
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    },
+    {
+      id: '2',
+      name: 'Fit T-Shirts',
+      price: 1300,
+      image:
+        'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    },
+    {
+      id: '3',
+      name: 'Cotton Jacket',
+      price: 1000,
+      image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+    },
+  ],
+}
+export default data
